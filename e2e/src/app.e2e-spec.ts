7ae8fb752e3c26b1fbb565bd7056c19a34adee3c
Aleksander Koshevierov <Iworb.ne@gmail.com>
2018-11-22 12:52:55 +0300

    e2e test fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display "Pictures" in appbar', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to pip-webui2-pictures-app!');
+    expect(page.getParagraphText()).toEqual('Pictures');
   });
 });
